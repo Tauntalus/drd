6c448201e79c9a14ceaf7c21779644630e2bae8c
Tauntalus <30677625+Tauntalus@users.noreply.github.com>
2022-06-12 19:14:08 -0700

    Added HTML helper functions

--- server.py
+++ server.py
@@ -4,6 +4,31 @@ import src.stoid
 
 # Server_DRD: Custom WebServer for Domain ReDirector
 class Server_DRD(BaseHTTPRequestHandler):
+    internal_db = {}
+    def send_ok_header(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    # send_head_html: Send the header information for the HTML page.
+    def send_head_html(self, name):
+        self.wfile.write(bytes(
+            """<head>
+                <title>%s</title>
+            </head>""" % name, "utf-8"))
+
+    def send_home_page(self):
+        self.send_ok_header()
+
+        self.wfile.write(bytes("<html>", "utf-8"))
+        self.send_head_html("Domain ReDirector - Main Page")
+        self.wfile.write(bytes(
+            """<body>
+                <p>This is the home page.</p>
+                <p>Thank you for visiting.</p>
+            </body>""", "utf-8"))
+        self.wfile.write(bytes("<html>", "utf-8"))
+
     def do_GET(self): # TODO: Transform into a link lookup
 
         # This line gets the path as a list of arguments,
@@ -11,24 +36,12 @@ class Server_DRD(BaseHTTPRequestHandler):
         args = self.path[1:].split('/')
 
         if args[0] == '' and len(args) == 1:
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            self.send_home_page()
 
-            self.wfile.write(bytes(
-                """<html>
-                    <head>
-                        <title>Domain ReDirector - Main Page</title>
-                    </head>
-                    <body>
-                        <p>This is the home page.</p>
-                        <p>Thank you for visiting.</p>
-                    </body>
-                </html>""", "utf-8"))
         elif args[0] == "stoid" and len(args) == 2:
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            self.send_ok_header()
+            self.wfile.write(bytes("<html>", "utf-8"))
+            self.send_head_html("Domain ReDirector - String Convertor")
 
             # Process string
             s = args[1]
@@ -36,31 +49,24 @@ class Server_DRD(BaseHTTPRequestHandler):
                 num = src.stoid.stoid(s.upper())
 
                 self.wfile.write(bytes(
-                    """<html>
-                        <head>
-                            <title>Domain ReDirector - String Convertor</title>
-                        </head>
-                        <body>
-                            <p>Your string: %s</p>
-                            <p>Its ID: %d</p>
-                        </body>
-                    </html>""" % (s.upper(), int(num)), "utf-8"))
+                    """<body>
+                        <p>Your string: %s</p>
+                        <p>Its ID: %d</p>
+                    </body>""" % (s.upper(), int(num)), "utf-8"))
+
+
             else:
                 self.wfile.write(bytes(
-                    """<html>
-                        <head>
-                            <title>Domain ReDirector - String Convertor</title>
-                        </head>
-                        <body>
-                            <p>The given string is not valid.</p>
-                        </body>
-                    </html>""", "utf-8"))
+                    """<body>
+                        <p>The given string is not valid.</p>
+                    </body>""", "utf-8"))
+
+            self.wfile.write(bytes("</html>", "utf-8"))
 
         elif args[0] == "idtos" and len(args) == 2:
-            # Send HTTP response
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            self.send_ok_header()
+            self.wfile.write(bytes("<html>", "utf-8"))
+            self.send_head_html("Domain ReDirector - ID Convertor")
 
             # Process ID
             num = args[1]
@@ -68,37 +74,24 @@ class Server_DRD(BaseHTTPRequestHandler):
                 s = src.stoid.idtos(int(num))
 
                 self.wfile.write(bytes(
-                    """<html>
-                        <head>
-                            <title>Domain ReDirector - ID Convertor</title>
-                        </head>
-                        <body>
-                            <p>Your ID: %d</p>
-                            <p>Its string: %s</p>
-                        </body>
-                    </html>""" % (int(num), s.upper()), "utf-8"))
+                    """<body>
+                        <p>Your ID: %d</p>
+                        <p>Its string: %s</p>
+                    </body>""" % (int(num), s.upper()), "utf-8"))
             else:
                 self.wfile.write(bytes(
-                    """<html>
-                        <head>
-                            <title>Domain ReDirector - ID Convertor</title>
-                        </head>
-                        <body>
-                            <p>The given ID is not valid.</p>
-                        </body>
-                    </html>""", "utf-8"))
-        else:
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+                    """<body>
+                        <p>The given ID is not valid.</p>
+                    </body>""", "utf-8"))
 
-            # Send HTML Page response
+            self.wfile.write(bytes("</html>", "utf-8"))
+
+        else:
+            self.send_ok_header()
+            self.wfile.write(bytes("<html>", "utf-8"))
+            self.send_head_html("Domain ReDirector - Other")
             self.wfile.write(bytes(
-                """<html>
-                    <head>
-                        <title>Domain ReDirector - Other</title>
-                    </head>
-                    <body>
-                        <p>This page does nothing.</p>
-                    </body>
-                </html>""", "utf-8"))
+                """<body>
+                    <p>This page does nothing.</p>
+                </body>""", "utf-8"))
+            self.wfile.write(bytes("</html>", "utf-8"))
