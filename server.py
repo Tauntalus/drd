5b856055e4087c2898757d90dfc90062ab6aaab9
Tauntalus <30677625+Tauntalus@users.noreply.github.com>
2022-06-13 19:52:22 -0700

    Fixed minor bug, added better console output

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 from src.stoid import stoid, idtos
 from src.database import LinkDB
-
+from urllib.parse import unquote
 
 # Server_DRD: Custom WebServer for Domain ReDirector
 class Server_DRD(BaseHTTPRequestHandler):
@@ -69,12 +69,32 @@ class Server_DRD(BaseHTTPRequestHandler):
                            <form method="POST" action="register-complete">
                             <div>
                                 <label for="link">Link to register: </label>
-                                <input type="text" id="link" name="link" required>
+                                <input type="url" id="link" name="link" required>
                             </div>
                             <div>
                                 <input type=submit value="Register">
                             </div>
                            </form>""")
+            return
+
+        elif args[0] == "register-id":
+            self.send_page(200, "Domain ReDirector - Register A Link",
+
+                           """<h1>Register a New Link With An ID</h1></br>
+                           <form method="POST" action="register-id-complete">
+                            <div>
+                                <label for="link">Link to register: </label>
+                                <input type="url" id="link" name="link" required>
+                            </div>
+                            <div>
+                                <label for="ext">5-letter ID: </label>
+                                <input type="text" id="ext" name="ext" 
+                                minlength=%d maxlength=%d pattern="[A-Za-z]{5}" required>
+                            </div>
+                            <div>
+                                <input type=submit value="Register">
+                            </div>
+                           </form>""" % (self.internal_db.char_limit, self.internal_db.char_limit))
             return
 
         elif args[0] == "stoid" and len(args) == 2:
@@ -133,17 +153,51 @@ class Server_DRD(BaseHTTPRequestHandler):
                             <a href="localhost:8080/%s">localhost:8080/%s</a>
                            </p>""" % (link, new_ext, new_ext))
             return
+        elif args[0] == "register-id-complete":
+            form_dict = self.process_form()
+            link = str(unquote(form_dict["link"]))
+            link_ext = str(form_dict["ext"])
 
+            if self.internal_db.get_id_by_link(link):
+                self.redirect("already-registered")
+                return
+
+            link_id = stoid(link_ext)
+            new_id = self.internal_db.add_with_id(link, link_id)
+            if new_id >= 0:
+
+                self.send_page(201, "Domain ReDirector - Registration Complete",
+                               """<h1>Registration Complete!</h1></br>
+                               <p>Thank you! Your link (%s) has been registered with the ID: %s!</p>
+                               <p>Your new short link is 
+                                <a href="localhost:8080/%s">localhost:8080/%s</a>
+                               </p>""" % (link, link_ext, link_ext, link_ext))
+                return
+            else:
+                self.redirect("id-taken")
+                return
+
+        # Soft Error Pages
+        # These pages handle "soft errors" - not sever enough to crash, but
         elif args[0] == "already-registered":
             form_dict = self.process_form()
-            link = form_dict["link"]
-
+            link = str(unquote(form_dict["link"]))
             cur_id = self.internal_db.get_id_by_link(link)
             cur_ext = idtos(cur_id)
+
             self.send_page(201, "Domain ReDirector - Link Already Registered",
                            """<h1>Your Link was Already Registered.</h1></br>
                            <p>Your link (%s) has already been registered in our database.</p>
                            <p>Its short link is 
                             <a href="localhost:8080/%s">localhost:8080/%s</a>
                            </p>""" % (link, cur_ext, cur_ext))
+            return
+
+        elif args[0] == "id-taken":
+            form_dict = self.process_form()
+            cur_ext = str(form_dict["ext"])
+
+            self.send_page(201, "Domain ReDirector - ID Taken",
+                           """<h1>Your ID Was Already Taken.</h1></br>
+                           <p>Your chosen ID (%s) was already registered in our database.</p>""" % (cur_ext))
             return
