a74b24b885b36270ba61c911c2299951d07d0099
Tauntalus <30677625+Tauntalus@users.noreply.github.com>
2022-06-14 21:47:50 -0700

    Added SQLite creation function

--- database.py
+++ database.py
@@ -1,6 +1,22 @@
 import stoid
 import random
+import sqlite3
+from sqlite3 import Error
 
+
+# try_create_db: Creates a DB file if none exists at <target>
+# If successful, returns the connection. Otherwise, returns None.
+def try_create_db(target):
+    conn = None
+    try:
+        conn = sqlite3.connect(target)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            return conn
+        else:
+            return None
 
 class LinkDB:
     char_limit = 3
