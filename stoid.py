963ee6b7c857a159f093cc41a1173b9be55c559f
Tauntalus <30677625+Tauntalus@users.noreply.github.com>
2022-06-15 20:15:39 -0700

    Made STOID module more generalized

--- stoid.py
+++ stoid.py
@@ -1,68 +1,42 @@
-CHAR_MAP = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-]
-
-MAP_LEN = len(CHAR_MAP)
 
 
 # stoid - String to ID conversion
-# strings are processed using CHAR_MAP to retrieve a decimal conversion of an alphadecimal number
-def stoid(s):
+def stoid(s, charset):
     s = s.upper()
+    base = len(charset)
     str_len = len(s)
     num = 0
 
     i = 0
     while i < str_len:
         char = s[i]
-        place_val = CHAR_MAP.index(char)
+        place_val = charset.index(char)
 
         # Take the exact place value, then multiply it by it's positional value.
         # The positional value ranges from str_len - 1 to 0
-        num += place_val * pow(MAP_LEN, str_len - i - 1)
+        num += place_val * pow(base, str_len - i - 1)
         i += 1
 
     return num
 
 
 # idtos - ID to String conversion
-# Translate
-def idtos(num):
+def idtos(num, charset, ret_size):
     num_temp = num
+    base = len(charset)
     s = ""
 
     # Build the string by repeated modulo and division of the base number
-    while num_temp >= MAP_LEN:
-        s = CHAR_MAP[num_temp % MAP_LEN] + s
-        num_temp -= num_temp % MAP_LEN
-        num_temp = int(num_temp / MAP_LEN)
+    while num_temp >= base:
+        s = charset[num_temp % base] + s
+        num_temp -= num_temp % base
+        num_temp = int(num_temp / base)
 
     # Final character is guaranteed to be between 0-25,
     # So we can just add it to the string
-    s = CHAR_MAP[num_temp % MAP_LEN] + s
+    s = charset[num_temp % base] + s
+
+    # Increase size of string if too short
+    while len(s) < ret_size:
+        s = charset[0] + s
     return s
