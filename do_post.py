7f195dfe2fae67d6960b1395ac602bc2bb870660
Tauntalus <30677625+Tauntalus@users.noreply.github.com>
2022-06-15 20:17:06 -0700

    Added stubs for all POST paths and SQL queries

--- do_post.py
+++ do_post.py
@@ -1,10 +1,12 @@
+import database
+
+from re import search
 from urllib.parse import urlparse, unquote
-from database import try_connect_db, try_create_table, close
-import stoid
+from stoid import stoid, idtos
 
 
 # validate_form_data: Checks that the data is formatted properly for processing
-def validate_form_data(form_data, ext_limit):
+def validate_form_data(form_data, charset, ext_limit):
     link = form_data.get("link")
     ext = form_data.get("ext")
 
@@ -20,25 +22,38 @@ def validate_form_data(form_data, ext_limit):
             print("Validator: Link is not a valid URL.")
             print("Validator: %(link)s" % {"link": link})
             link_flag = -1
-
+    else:
+        print("Validator: No link in form data.")
+        link_flag = 1
     # Character ID validation
     if ext:
         ext = str(ext)
-        if not ext.isalpha() or 0 >= len(ext) > ext_limit:
+        regex = "[" + charset + "]+"
+        if not search(regex, ext) or 0 >= len(ext) > ext_limit:
             print("Validator: Extension is not a valid character sequence.")
             print("Validator: %(ext)s" % {"ext": ext})
             ext_flag = -1
-
+    else:
+        print("Validator: No extension in form data.")
+        ext_flag = 1
     return link_flag, ext_flag
 
+
 # TODO: Move HTML pages to external resource
-def handle_post(args, form_data, db_info, id_limit):
+def handle_post(args, form_data, charset, id_limit):
+    # TODO: Improve information control on DB schemas
+    db_file = "db/link.db"
+    db_table = "link"
+    db_fields = ("id", "link")
+
+    # Default to 404
     code = 404
     title = "Page Not Found"
     body = "It looks like the page you're looking for doesn't exist."
     inserts = {}
 
-    link_flag, ext_flag = validate_form_data(form_data, id_limit)
+    # Validation of form data
+    link_flag, ext_flag = validate_form_data(form_data, charset, id_limit)
     if link_flag < 0:
         code = 308
         body = "link-invalid"
@@ -47,24 +62,97 @@ def handle_post(args, form_data, db_info, id_limit):
         code = 308
         body = "id-invalid"
 
-    if len(args) > 0:
-        conn = try_connect_db(db_info.file)
-        if conn:
-            conn = try_create_table(conn, db_info.tables, db_info.schemas)
+    else:
+        if len(args) > 0:
+            conn = database.try_connect_db(db_file)
             if conn:
-                if args[0] == "register":
-                    conn, ret =
-                    pass
+                conn = database.try_create_table(conn, db_table, db_fields)
+                if conn:
+                    ret = None
+                    link = None
+                    link_id = None
+
+                    if not link_flag:
+                        link = str(unquote(form_data["link"]))
+                    if not ext_flag:
+                        link_id = stoid(str(form_data["ext"]), charset)
+
+                    # At this point, we've established that the data is well-ordered,
+                    # but requests can still be pointed in the wrong directions or
+                    # maliciously omit fields. Moving forward,
+                    # the response defaults to "bad request"
+                    code = 400
+                    title = "Bad POST Request"
+                    body = "The form data we received from you doesn't match what we were expecting."
+
+                    # Anyways,
+                    # with the DB connection established,
+                    # it's time to start making queries!
+                    if args[0] == "register":
+                        if not link_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE link=?",
+                                                             link)
+                    elif args[0] == "register-id":
+                        if not link_flag and not ext_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE link=? OR id=?",
+                                                             (link, link_id))
+                    elif args[0] == "remove":
+                        if not link_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE link=?",
+                                                             link)
+                    elif args[0] == "remove-id":
+                        if not ext_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE id=?",
+                                                             link_id)
+                    elif args[0] == "update":
+                        if not link_flag and not ext_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE link=? OR id=?",
+                                                             (link, link_id))
+                    elif args[0] == "update-id":
+                        if not link_flag and not ext_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE link=? OR id=?",
+                                                             (link, link_id))
+
+                    # Soft Error Pages
+                    # These pages handle "soft errors" - not severe enough to crash the server,
+                    # but notable enough to deserve a response to the client
+                    elif args[0] == "link-registered":
+                        pass
+                    elif args[0] == "link-not-found":
+                        pass
+                    elif args[0] == "link-invalid":
+                        pass
+                    elif args[0] == "id-registered":
+                        pass
+                    elif args[0] == "id-not-found":
+                        pass
+                    elif args[0] == "id-invalid":
+                        pass
+                    elif args[0] == "bad-request":
+                        pass
+
+                    # Sanity check - if return is None, DB Query broke and we need to stop
+                    if not ret:
+                        code = 500
+                        title = "Internal Error - Invalid Database Response"
+                        body = """The database we use is responding erratically. We've logged what happened and
+                               are looking into why it's happened."""
+                else:
+                    code = 500
+                    title = "Internal Error - Database Table Inaccessible"
+                    body = "We failed to access our database table. We can't do anything without that!"
+                conn = database.close(conn)
             else:
                 code = 500
-                title = "Internal Error - Database Table Inaccessible"
-                body = "We failed to access our database table. We can't do anything without that!"
-        else:
-            code = 500
-            title = "Internal Error - Database Failed To Initialize"
-            body = "The connection to our database failed to initialize. We can't do anything without that!"
+                title = "Internal Error - Database Failed To Initialize"
+                body = "The connection to our database failed to initialize. We can't do anything without that!"
 
-        close(conn)
     return code, title, body, inserts
 
             if args[0] == "register":
