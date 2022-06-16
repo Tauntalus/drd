1e0f1a2a260d65a429de1451425c3a48c82c819b
Tauntalus <30677625+Tauntalus@users.noreply.github.com>
2022-06-15 23:18:07 -0700

    Added soft error pages

--- do_post.py
+++ do_post.py
@@ -74,7 +74,8 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                     if not link_flag:
                         link = str(unquote(form_data["link"]))
                     if not ext_flag:
-                        link_id = stoid(str(form_data["ext"]), charset)
+                        link_ext = str(form_data["ext"]).upper()
+                        link_id = stoid(link_ext, charset)
 
                     # At this point, we've established that the data is well-ordered,
                     # but requests can still be pointed in the wrong directions or
@@ -116,8 +117,6 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                                 conn = database.try_insert(conn, "links", (link_id, link))
 
                                 # Finally, build the HTML response
-                                link_ext = idtos(link_id, charset, id_limit)
-
                                 code = 201
                                 title = "Registration Complete"
                                 body = """
@@ -145,7 +144,6 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                                     body = "id-registered"
                             else:
                                 conn, ret = database.try_insert(conn, "links", (link_id, link))
-                                link_ext = idtos(link_id, charset, id_limit)
                                 code = 201
                                 title = "Registration Complete"
                                 body = """
@@ -188,7 +186,6 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                                 conn, ret = database.try_execute(conn,
                                                             "DELETE FROM links WHERE id=?",
                                                             link_id)
-                                link_ext = idtos(link_id, charset, id_limit)
                                 code = 201
                                 title = "Removal Complete",
                                 body = """
@@ -212,7 +209,6 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                                     conn, ret = database.try_execute(conn,
                                                                      "UPDATE links SET id=?, link=? WHERE link=?",
                                                                      (link_id, link, link))
-                                    link_ext = idtos(link_id, charset, id_limit)
                                     code = 201
                                     title = "Update Complete"
                                     body = """
@@ -239,7 +235,6 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                                     conn, ret = database.try_execute(conn,
                                                                      "UPDATE links SET id=?, link=? WHERE id=?",
                                                                      (link_id, link, link_id))
-                                    link_ext = idtos(link_id, charset, id_limit)
                                     code = 201
                                     title = "Update Complete"
                                     body = """
@@ -254,19 +249,83 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                     # These pages handle "soft errors" - not severe enough to crash the server,
                     # but notable enough to deserve a response to the client
                     elif args[0] == "link-registered":
-                        pass
+                        if not link_flag:
+                            conn, ret = database.try_execute(conn,
+                                                             "SELECT * FROM links WHERE link=?",
+                                                             link)
+                            link_id = ret[0][0]
+                            link_ext = idtos(link_id)
+
+                            code = 201
+                            title = "Link Already Registered",
+                            body = """
+                            <h2>Your Link Was Already Registered.</h2><br>
+                            <p>Your link <b>(%(link)s)</b> has already been registered in our database.</p>
+                            <p>Its short link is 
+                                <a href="%(host)s/%(ext)s"><b>%(host)s/%(ext)s</b></a>
+                            </p>
+                            <button onclick="history.back()">Go Back</button>
+                            """
+                            inserts["link"] = link
+                            inserts["ext"] = link_ext
+
                     elif args[0] == "link-not-found":
-                        pass
+                        if not link_flag:
+                            code = 201
+                            title = "Link Not Found"
+                            body ="""
+                            <h2>Your Link Was Not Found.</h2><br>
+                            <p>The link you are trying to modify <b>(%(link)s)</b> does not appear to be in our database.</p>
+                            <button onclick="history.back()">Go Back</button>
+                            """
+                            inserts["link"] = link
+
                     elif args[0] == "link-invalid":
-                        pass
+                        if not link_flag:
+                            code = 201
+                            title = "Link Invalid"
+                            body = """
+                            <h2>Your Link Was Invalid.</h2><br>
+                            <p>Your link <b>(%(link)s)</b> was invalid.</p>
+                            <button onclick="history.back()">Go Back</button>
+                            """
+                            inserts["link"] = link
+
                     elif args[0] == "id-registered":
-                        pass
+                        if not ext_flag:
+                            code = 201
+                            title = "ID Already Registered"
+                            body = """<h2>Your ID Was Already Registered.</h2><br>
+                            <p>Your chosen ID <b>(%(ext)s)</b> was already registered
+                             in our database. Sorry about that.</p>
+                            <button onclick="history.back()">Go Back</button>
+                            """
+                            inserts["ext"] = link_ext
+
                     elif args[0] == "id-not-found":
-                        pass
+                        if not ext_flag:
+                            code = 201
+                            title = "ID Not Found"
+                            body = """
+                            <h2>Your ID Was Not Found.</h2><br>
+                            <p>The ID you are trying to modify <b>(%(ext)s)</b>
+                            doesn't seem to exist in our database.</p>
+                            <button onclick="history.back()">Go Back</button>
+                            """
+                            inserts["ext"] = link_ext
+
                     elif args[0] == "id-invalid":
-                        pass
-                    elif args[0] == "bad-request":
-                        pass
+                        if not ext_flag:
+                            code = 201
+                            title = "ID Invalid"
+                            body = """
+                            <h2>Your ID Was Invalid.</h2><br>
+                            <p>Your chosen ID <b>(%(ext)s)</b> was invalid. It could be that
+                            the ID was too long or short, or contained characters
+                            other than letters.</p>
+                            <button onclick="history.back()">Go Back</button>
+                            """
+                            inserts["ext"] = link_ext
 
                     # Sanity check - if return is None, DB Query broke and we need to stop
                     if not ret:
@@ -286,205 +345,3 @@ def handle_post(args, form_data, charset, id_limit, max_fails):
                 body = "The connection to our database failed to initialize. We can't do anything without that!"
 
     return code, title, body, inserts, fail_flag
-
-            if args[0] == "register":
-                if self.internal_db.get_id_by_link(link):
-                    self.redirect(308, "link-already-registered")
-                    return
-
-                link_id = self.internal_db.add_rand(link)
-                link_ext = idtos(link_id)
-
-                self.send_page(201, "Registration Complete",
-                               """<h2>Registration Complete!</h2><br>
-                               <p>Thank you! Your link <b>(%(link)s)</b> has been registered!</p>
-                               <p>Your new short link is 
-                                <a href="%(host)s/%(ext)s" target="_blank"><b>%(host)s/%(ext)s</b></a>
-                               </p>"""
-                               % {"link": link, "host": self.get_server_address(), "ext": link_ext})
-                return
-            elif args[0] == "register-id-complete":
-                form_dict = self.process_form()
-                link = str(unquote(form_dict["link"]))
-                link_ext = str(form_dict["ext"]).upper()
-                if self.internal_db.get_id_by_link(link):
-                    self.redirect(308, "link-already-registered")
-                    return
-
-                link_id = stoid(link_ext)
-                link_id = self.internal_db.add_with_id(link, link_id)
-                if link_id >= 0:
-
-                    self.send_page(201, "Registration Complete",
-                                   """<h2>Registration Complete!</h2><br>
-                                   <p>Thank you! Your link <b>(%(link)s)</b> has been registered with the ID: %(ext)s!</p>
-                                   <p>Your new short link is 
-                                <a href="%(host)s/%(ext)s" target="_blank"><b>%(host)s/%(ext)s</b></a>
-                               </p>"""
-                                   % {"link": link, "host": self.get_server_address(), "ext": link_ext})
-                    return
-                elif link_id == -1:
-                    self.redirect(308, "id-taken")
-                    return
-                elif link_id == -2:
-                    self.redirect(308, "id-invalid")
-                    return
-                # TODO: Add 500 server error catch
-
-            elif args[0] == "remove-complete":
-                form_dict = self.process_form()
-                link = str(unquote(form_dict["link"]))
-                link_id = self.internal_db.remove_by_link(link)
-
-                if link_id >= 0:
-                    self.send_page(201, "Removal Complete",
-                                   """<h2>Removal Complete!</h2><br>
-                                   <p>The link <b>(%(link)s)</b> has been removed from our database.</p>"""
-                                   % {"link": link})
-                    return
-                else:
-                    self.redirect(308, "link-not-found")
-                    return
-
-            elif args[0] == "remove-id-complete":
-                form_dict = self.process_form()
-                link_ext = str(form_dict["ext"]).upper()
-
-                link_id = stoid(link_ext)
-                old_id = self.internal_db.remove_by_id(link_id)
-                if old_id >= 0:
-                    self.send_page(201, "Removal Complete",
-                                   """<h2>Removal Complete!</h2><br>
-                                   <p>The ID <b>(%(ext)s)</b> has been unregistered from our database.</p>"""
-                                   % {"ext": link_ext})
-                    return
-                elif old_id == -1:
-                    self.redirect(308, "id-not-found")
-                    return
-                elif old_id == -2:
-                    self.redirect(308, "id-invalid")
-                    return
-                # TODO: Add 500 server error catch
-
-            elif args[0] == "update-complete":
-                form_dict = self.process_form()
-                link = str(unquote(form_dict["link"]))
-                link_ext = str(form_dict["ext"]).upper()
-                link_id = stoid(link_ext)
-                link_id = self.internal_db.update_by_link(link, link_id)
-                if link_id >= 0:
-                    self.send_page(201, "Update Complete",
-                                   """<h2>Update Complete!</h2><br>
-                                   <p>The link <b>(%(link)s)</b> has been moved to the address 
-                                   <a href=%(addr)s target="_blank"><b>%(addr)s</b></a>.</p>"""
-                                   % {"link": link, "addr": self.get_server_address() + "/" + link_ext})
-                    return
-                elif link_id == -1:
-                    self.redirect(308, "id-taken")
-                    return
-                elif link_id == -2:
-                    self.redirect(308, "id-invalid")
-                    return
-                elif link_id == -3:
-                    self.redirect(308, "link-not-found")
-                    return
-
-            elif args[0] == "update-id-complete":
-                form_dict = self.process_form()
-                link = str(unquote(form_dict["link"]))
-                link_ext = str(form_dict["ext"]).upper()
-                link_id = stoid(link_ext)
-                old_id = self.internal_db.update_by_link(link, link_id)
-                if old_id >= 0:
-                    self.send_page(201, "Update Complete",
-                                   """<h2>Update Complete!</h2><br>
-                                   <p>The link <b>(%(link)s)</b> has been moved to the ID: <b>%(ext)s</b>.</p>
-                                   <p>Its new Shortlink is: 
-                                   <a href="%(addr)s" target="blank"><b>%(addr)s</b></a>.</p>"""
-                                   % {"link": link, "ext": link_ext,
-                                      "addr": self.get_server_address() + "/" + link_ext})
-                    return
-                elif old_id == -1:
-                    self.redirect(308, "id-not-found")
-                    return
-                elif old_id == -2:
-                    self.redirect(308, "id-invalid")
-                    return
-                # TODO: Add 500 server error catch
-
-            # Soft Error Pages
-            # These pages handle "soft errors" - not severe enough to crash the server,
-            # but notable enough to deserve a response to the client
-            elif args[0] == "link-already-registered":
-                form_dict = self.process_form()
-                link = str(unquote(form_dict["link"]))
-                link_id = self.internal_db.get_id_by_link(link)
-                link_ext = idtos(link_id)
-
-                self.send_page(201, "Link Already Registered",
-                               """<h2>Your Link Was Already Registered.</h2><br>
-                               <p>Your link <b>(%(link)s)</b> has already been registered in our database.</p>
-                               <p>Its short link is 
-                                <a href="%(host)s/%(ext)s"><b>%(host)s/%(ext)s</b></a>
-                               </p>
-                               <button onclick="history.back()">Go Back</button>
-                                """
-                               % {"link": link, "host": self.get_server_address(), "ext": link_ext})
-                return
-
-            elif args[0] == "link-not-found":
-                form_dict = self.process_form()
-                link = str(unquote(form_dict["link"]))
-
-                self.send_page(201, "Link Not Found",
-                               """<h2>Your Link Was Not Found.</h2><br>
-                               <p>The link you are trying to modify <b>(%(link)s)</b> does not appear to be in our database.</p>
-                               <button onclick="history.back()">Go Back</button>
-                                """
-                               % {"link": link})
-                return
-
-            elif args[0] == "id-taken":
-                form_dict = self.process_form()
-                link_ext = str(form_dict["ext"]).upper()
-
-                self.send_page(201, "ID Taken",
-                               """<h2>Your ID Was Already Registered.</h2><br>
-                               <p>Your chosen ID <b>(%(ext)s)</b> was already registered
-                               in our database. Sorry about that.</p>
-                               <button onclick="history.back()">Go Back</button>
-                                """
-                               % {"ext": link_ext})
-                return
-
-            elif args[0] == "id-not-found":
-                form_dict = self.process_form()
-                link_ext = str(unquote(form_dict["ext"])).upper()
-
-                self.send_page(201, "ID Not Found",
-                               """<h2>Your ID Was Not Found.</h2><br>
-                               <p>The ID you are trying to modify <b>(%(ext)s)</b>
-                                doesn't seem to exist in our database.</p>
-                               <button onclick="history.back()">Go Back</button>
-                                """
-                               % {"ext": link_ext})
-                return
-
-            elif args[0] == "id-invalid":
-                form_dict = self.process_form()
-                link_ext = str(form_dict["ext"]).upper()
-                self.send_page(201, "ID Invalid",
-                               """<h2>Your ID Was Invalid.</h2><br>
-                               <p>Your chosen ID <b>(%(ext)s)</b> was invalid. It could be that
-                                the ID was too long or short, or contained characters
-                                other than letters.</p>
-                                <button onclick="history.back()">Go Back</button>
-                                """
-                               % {"ext": link_ext})
-                return
-
-            # If nothing catches a wayward request, send it to the 404 page.
-            self.send_error(404,
-                            "Page Not Found",
-                            "It looks like the page you're looking for doesn't exist.")
-            return
